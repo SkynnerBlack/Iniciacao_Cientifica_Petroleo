cbcf6c827f7c18d77914d2f26163569b1ec9ae92
Gabriel Andrade do Nascimento <gabrielandradedonascimento963@gmail.com>
2024-08-22 08:46:44 -0300

    Adicionando mais tansformaçoes de vairáveis

--- Scripts/Unificador.py
+++ Scripts/Unificador.py
@@ -27,7 +27,7 @@ for year in range(2020, 2024):
         full_path = f"{path_combustivel_automotivo}\\{file_name}.csv"
         # Ler o arquivo CSV e armazenar no dicionário
         dict_combustivel_automotivo[file_name] = (
-            pd.read_csv(full_path, sep=";", encoding='latin-1'))
+            pd.read_csv(full_path, sep=";", encoding='latin1'))
         
 # 1.1. Importando os datasets de GLP
 
@@ -43,37 +43,47 @@ for year in range(2020, 2024):
         full_path = f"{path_GLP_P13}\\{file_name}.csv"
         # Ler o arquivo CSV e armazenar no dicionário
         dict_GLP_P13[file_name] = (
-            pd.read_csv(full_path, sep=";", encoding='latin-1'))
+            pd.read_csv(full_path, sep=";", encoding='utf-8-sig'))
 
 # 1.2. Importando os datasets de Cotacao dolar
 
 # Define o caminho base para os arquivos
-path_Cotacao_dolar = path_insumos + r"\Cotacao dolar"
-dict_Cotacao_dolar = {} 
+path_Cotacao = path_insumos + r"\Cotacao"
+dict_cotacao = {} 
 
-for year in range(2020, 2024):
-    for sem in range(1, 3):
-        # Criar a chave do dicionário como o nome do arquivo
-        file_name = f"Cotacao dolar - {year}.0{sem}"
-        # Construir o caminho completo do arquivo
-        full_path = f"{path_Cotacao_dolar}\\{file_name}.csv"
-        # Ler o arquivo CSV e armazenar no dicionário
-        dict_Cotacao_dolar[file_name] = (
-            pd.read_csv(full_path, sep=";", encoding='latin-1'))
+# Para cada uma das pastas de petrolíferas
+for Moeda in os.listdir(path_Cotacao):
+    path_Moeda = os.path.join(path_Cotacao, Moeda)
 
-# 1.3. Importando os datasets de PETR
+    # Cria uma lista vazia para as bases dentro dela
+    lista_databases_moedas = []
+    
+    # Para cada uma das pastas, lê o arquivo e adiciona-o na lista do respectivo dicionário
+    for database in os.listdir(path_Moeda):
+        file_path = os.path.join(path_Moeda, database)
+        lista_databases_moedas.append(pd.read_csv(file_path,sep=";", encoding='utf-8-sig'))
+    
+    dict_cotacao[Moeda] = lista_databases_moedas
+
+# 1.3. Importando os datasets de Petroleo
 
 # Define o caminho base para os arquivos
-path_PETR = path_insumos + r"\PETR"
-dict_PETR = {} 
+path_petroleo = path_insumos + r"\Petroleo"
+dict_petroleo = {} 
 
-for affix in [3, 4]:
-    # Criar a chave do dicionário como o nome do arquivo
-    file_name = f"PETR{affix}"
-    # Construir o caminho completo do arquivo
-    full_path = f"{path_PETR}\\{file_name}.csv"
-    # Ler o arquivo CSV e armazenar no dicionário
-    dict_PETR[file_name] = pd.read_csv(full_path, encoding='latin-1')
+# Para cada uma das pastas de petrolíferas
+for petrolifera in os.listdir(path_petroleo):
+    path_petrolifera = os.path.join(path_petroleo, petrolifera)
+
+    # Cria uma lista vazia para as bases dentro dela
+    lista_databases_petroliferas = []
+    
+    # Para cada uma das pastas, lê o arquivo e adiciona-o na lista do respectivo dicionário
+    for database in os.listdir(path_petrolifera):
+        file_path = os.path.join(path_petrolifera, database)
+        lista_databases_petroliferas.append(pd.read_csv(file_path, sep=",", encoding='utf-8-sig'))
+    
+    dict_petroleo[petrolifera] = lista_databases_petroliferas
 
 # --- #
 
@@ -143,91 +153,98 @@ for df_name, df in dict_GLP_P13.items():
     # Armazenar o DataFrame sumarizado no novo dicionário
     dict_GLP_sumarizado[sumarized_file_name] = summarized_df
 
-# 2.2. Tratando as bases de Cotacao dolar
+# 2.2. Tratando as bases de Cotacao
 
 dict_cotacao_sumarizado = {}
 
-for df_name, df in dict_Cotacao_dolar.items():
-    # Cria o nome do arquivo sumarizado com base no nome original
-    sumarized_file_name = (df_name.split("202")[0] + "sumarizado 202" + df_name.split("202")[1])
+for moeda, lista_database in dict_cotacao.items():
 
-    # Transpor o DataFrame
-    df = df.transpose()
+    lista_database_moedas_sumarizado = []
 
-    # Inserir a primeira linha com os nomes originais das colunas
-    df.insert(0, 'Variável', df.index)
+    for df in lista_database:
+        
+        # Transpor o DataFrame
+        df = df.transpose()
 
-    # Resetar os índices
-    df.reset_index(drop=True, inplace=True)
+        # Inserir a primeira linha com os nomes originais das colunas
+        df.insert(0, 'Variável', df.index)
 
-    # Transpor o DataFrame
-    df = df.transpose()
-    
-    # Selecionando as colunas 
-    summarized_df = df[[0, 4, 5]]
+        # Resetar os índices
+        df.reset_index(drop=True, inplace=True)
 
-    # Renomeando as colunas
-    summarized_df = summarized_df.rename(columns={0: 'Data da Coleta', 
-                                                  4: 'Cotação Dolar em Real (Compra)', 
-                                                  5: 'Cotação Dolar em Real (Venda)'})
-    
-    # Resetar o índice das linhas
-    summarized_df.reset_index(drop=True, inplace=True)
+        # Transpor o DataFrame
+        df = df.transpose()
+        
+        # Selecionando as colunas 
+        summarized_df = df[[0, 4, 5]]
 
-    # Transformando a coluna "Data de Coleta em string"
-    summarized_df['Data da Coleta'] = summarized_df['Data da Coleta'].astype(str)
+        # Renomeando as colunas
+        summarized_df = summarized_df.rename(columns={0: f'Data da Coleta', 
+                                                    4: f'Cotação {moeda.replace("Cotacao ", "")} em Real (Compra)', 
+                                                    5: f'Cotação {moeda.replace("Cotacao ", "")} em Real (Venda)'})
+        
+        # Resetar o índice das linhas
+        summarized_df.reset_index(drop=True, inplace=True)
 
-    # Adicionar um zero no começo da coluna "Data de Coleta" se estiver com 7 dígitos
-    summarized_df['Data da Coleta'] = summarized_df['Data da Coleta'].apply(lambda x: '0' + x if len(x) == 7 else x)
+        # Transformando a coluna "Data de Coleta em string"
+        summarized_df['Data da Coleta'] = summarized_df['Data da Coleta'].astype(str)
 
+        # Adicionar um zero no começo da coluna "Data de Coleta" se estiver com 7 dígitos
+        summarized_df['Data da Coleta'] = summarized_df['Data da Coleta'].apply(lambda x: '0' + x if len(x) == 7 else x)
 
-    # Convertendo a data para o formato AAAAMMDD
-    summarized_df['Data da Coleta'] = pd.to_datetime(summarized_df['Data da Coleta'], errors='coerce', format='%d%m%Y').dt.strftime('%Y%m%d')
+        # Convertendo a data para o formato AAAAMMDD
+        summarized_df['Data da Coleta'] = pd.to_datetime(summarized_df['Data da Coleta'], errors='coerce', format='%d%m%Y').dt.strftime('%Y%m%d')
 
-    # Convertendo e limpando dados de cotações
-    summarized_df['Cotação Dolar em Real (Compra)'] = summarized_df['Cotação Dolar em Real (Compra)'].str.replace(',', '.').astype(float)
-    summarized_df['Cotação Dolar em Real (Venda)'] = summarized_df['Cotação Dolar em Real (Venda)'].str.replace(',', '.').astype(float)
+        # Convertendo e limpando dados de cotações
+        summarized_df[f'Cotação {moeda.replace("Cotacao ", "")} em Real (Compra)'] = summarized_df[f'Cotação {moeda.replace("Cotacao ", "")} em Real (Compra)'].str.replace(',', '.').astype(float)
+        summarized_df[f'Cotação {moeda.replace("Cotacao ", "")} em Real (Venda)'] = summarized_df[f'Cotação {moeda.replace("Cotacao ", "")} em Real (Venda)'].str.replace(',', '.').astype(float)
 
-    # Armazenar o DataFrame sumarizado no novo dicionário
-    dict_cotacao_sumarizado[sumarized_file_name] = summarized_df
+        lista_database_moedas_sumarizado.append(summarized_df)
+
+        # Armazenar o DataFrame sumarizado no novo dicionário
+    dict_cotacao_sumarizado[moeda] = lista_database_moedas_sumarizado
 
 # 2.3. Tratando as bases de de PETR
-dict_PETR_sumarizado = {}
 
-for df_name, df in dict_PETR.items():
+dict_petroleo_sumarizado = {}
 
-    sumarized_file_name = (df_name + " - sumarizado")
+for petrolifera, lista_database in dict_petroleo.items():
 
-    # Renomear as colunas pela posição, pq seus nomes estão "quebrados"
-    df.columns.values[0] = f"Data da Coleta"
-    df.columns.values[1] = f"Fechamento {df_name} (em Reais)"
-    df.columns.values[5] = f"Volume de transações {df_name} (Em milhares)"
+    lista_database_petroliferas_sumarizado = []
+
+    for df in lista_database:
+
+        # Renomear as colunas pela posição, pq seus nomes estão "quebrados"
+        df.columns.values[0] = f"Data da Coleta"
+        df.columns.values[1] = f"Fechamento {petrolifera} (em Reais)"
+        df.columns.values[5] = f"Volume de transações {petrolifera} (Em milhares)"
+        
+        # Selecionando as colunas necessárias
+        summarized_df = df[[f"Data da Coleta", 
+                            f"Fechamento {petrolifera} (em Reais)", 
+                            f"Volume de transações {petrolifera} (Em milhares)"]]
+
+        # Formatação da coluna "Data da Coleta"
+        summarized_df["Data da Coleta"] = pd.to_datetime(summarized_df["Data da Coleta"], format="%d.%m.%Y").dt.strftime("%Y%m%d")
+
+        # Remover letras e converter a coluna "Volume de transações" para float
+        summarized_df[f"Volume de transações {petrolifera} (Em milhares)"] = (
+            summarized_df[f"Volume de transações {petrolifera} (Em milhares)"]
+            .str.replace(r'[^\d,]', '', regex=True)  # Remove letras
+            .str.replace(',', '.')  # Troca vírgula por ponto
+            .astype(float)  # Converte para float
+        )
+
+        # Converter a coluna "Fechamento {df_name} (em Reais)" para float
+        summarized_df[f"Fechamento {petrolifera} (em Reais)"] = (
+            summarized_df[f"Fechamento {petrolifera} (em Reais)"]
+            .str.replace(',', '.')  # Troca vírgula por ponto
+            .astype(float)  # Converte para float
+        )
+
+        lista_database_petroliferas_sumarizado.append(summarized_df)
     
-    # Selecionando as colunas necessárias
-    summarized_df = df[[f"Data da Coleta", 
-                        f"Fechamento {df_name} (em Reais)", 
-                        f"Volume de transações {df_name} (Em milhares)"]]
-
-    # Formatação da coluna "Data da Coleta"
-    summarized_df["Data da Coleta"] = pd.to_datetime(summarized_df["Data da Coleta"], format="%d.%m.%Y").dt.strftime("%Y%m%d")
-
-    # Remover letras e converter a coluna "Volume de transações" para float
-    summarized_df[f"Volume de transações {df_name} (Em milhares)"] = (
-        summarized_df[f"Volume de transações {df_name} (Em milhares)"]
-        .str.replace(r'[^\d,]', '', regex=True)  # Remove letras
-        .str.replace(',', '.')  # Troca vírgula por ponto
-        .astype(float)  # Converte para float
-    )
-
-    # Converter a coluna "Fechamento {df_name} (em Reais)" para float
-    summarized_df[f"Fechamento {df_name} (em Reais)"] = (
-        summarized_df[f"Fechamento {df_name} (em Reais)"]
-        .str.replace(',', '.')  # Troca vírgula por ponto
-        .astype(float)  # Converte para float
-    )
-
-    # Armazenar o DataFrame sumarizado no novo dicionário
-    dict_PETR_sumarizado[sumarized_file_name] = summarized_df
+    dict_petroleo_sumarizado[petrolifera] = lista_database_petroliferas_sumarizado
     
 # 3.0. Concatenando todos os dataframes verticalmente
 
@@ -236,6 +253,13 @@ df_combustivel_concatenado = df_combustivel_concatenado.sort_values(by='Data da 
 
 df_GLP_concatenado = pd.concat(dict_GLP_sumarizado.values())
 df_GLP_concatenado = df_GLP_concatenado.sort_values(by='Data da Coleta', ascending=True)
+
+dict_petrolifera_concatenado = {}
+for petrolifera, database_list in dict_petroleo_sumarizado.items():
+    dict_petrolifera_concatenado[petrolifera] = pd.concat(database_list, axis=0)
+    dict_petrolifera_concatenado[petrolifera].reset_index(drop=True, inplace=True)
+for petrolifera in list(dict_petrolifera_concatenado.values())[1:]:
+    df_petrolifera_concatenado = pd.merge(df_petrolifera_concatenado, petrolifera, on='key', how='outer')
 
 df_cotacao_concatenado = pd.concat(dict_cotacao_sumarizado.values())
 df_cotacao_concatenado = df_cotacao_concatenado.sort_values(by='Data da Coleta', ascending=True)
